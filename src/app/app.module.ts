33f48f1e279d49344a3344ca9fbf1e4952632fcc
AbdurRahamn M Zaghloul <drabdohza@gmail.com>
2023-02-20 23:59:32 +0200

    add anguluar flexlayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { CurrentWeatherComponent } from './current-weather/current-weather.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './material.module';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { AppComponent } from './app.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { MaterialModule } from './material.module'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CurrentWeatherComponent
-  ],
+  declarations: [AppComponent, CurrentWeatherComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
