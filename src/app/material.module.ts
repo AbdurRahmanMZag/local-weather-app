42230a3c0bcb09862e24707c7547534e2c07bd16
AbdurRahamn M Zaghloul <drabdohza@gmail.com>
2023-02-22 16:59:56 +0200

    Implementing layout scaffolding

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { NgModule } from '@angular/core';
-const modules = [MatButtonModule,MatIconModule,MatToolbarModule];
+const modules = [MatButtonModule, MatIconModule, MatToolbarModule, MatCardModule]
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    modules
-  ],
-  exports: modules
+  imports: [CommonModule, modules],
+  exports: modules,
 })
-export class MaterialModule { }
+export class MaterialModule {}
